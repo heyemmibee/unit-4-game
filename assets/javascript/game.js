4067b288100866134a51b3ef9d9b2ae9c2d06fbd
eburnaman <eburnaman@gmail.com>
2019-03-18 13:31:30 -0400

    skeleton is forming

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+//welcome to my javascript (attempt)
+
+//step 1, define the variables
+var ruby = 0;
+var sapphire = 0;
+var diamond = 0;
+var amesthyst = 0;
+var total
+
+
+//step 2, create randomization of crystal values and total
+
